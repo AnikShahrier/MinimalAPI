e31ae1a8f84777e11c31cf4b83074a1764fba7db
NLCD329 <anikshahrieroishik@gmail.com>
2022-08-26 20:54:40 +0600

    Done Making a minimal API

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using MinimalAPI;
 
@@ -20,5 +21,47 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+async Task<List<SuperHero>> GetAllHeroes(DataContext context) => await context.SuperHeroes.ToListAsync();
+app.MapGet("/superhero", async (DataContext context) => await context.SuperHeroes.ToListAsync());
+app.MapGet("/superhero/{id}", async (DataContext context, int id) =>
+    await context.SuperHeroes.FindAsync(id) is SuperHero hero ? Results.Ok(hero) :
+    Results.NotFound("Sorry, Not found"));
+app.MapPost("/superhero", async (DataContext context, SuperHero hero) =>
+{
+    context.SuperHeroes.Add(hero);
+    await context.SaveChangesAsync();
+    return Results.Ok(await GetAllHeroes(context));
+}
+
+);
+
+app.MapPut("/superhero/{id}", async (DataContext context, SuperHero hero, int id) =>
+{
+    var dbHero = await context.SuperHeroes.FindAsync(id);
+    if (dbHero == null) return Results.NotFound("No Hero Found");
+
+
+    dbHero.Name = hero.Name;
+    dbHero.FirstName = hero.FirstName; 
+    dbHero.LastName = hero.LastName;
+    dbHero.Place = hero.Place;
+
+    await context.SaveChangesAsync();
+    return Results.Ok(await GetAllHeroes(context));
+}
+);
+
+app.MapDelete("/superhero/{id}", async (DataContext context, int id) =>
+{
+    var dbHero = await context.SuperHeroes.FindAsync(id);
+    if (dbHero == null) return Results.NotFound("No Hero Found");
+
+    context.SuperHeroes.Remove(dbHero);
+    await context.SaveChangesAsync();
+    return Results.Ok(await GetAllHeroes(context));
+
+}
+);
 
 app.Run();
